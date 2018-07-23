b853f21f1fd5c80b6c552201cbd999c8a5aeff84
zhongwenfafafa <zwalvyuan456@gmail.com>
2018-07-23 06:34:13 +0000

    Finish layout and routes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <title>@yield('title', 'Sample App') - Laravel</title>
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
 </head>
 <body>
-    @yield('content')
+    @include('layouts._header')
+
+    <div class="container">
+        <div class="col-md-offset-1 col-md-10">
+            @yield('content')
+            @include('layouts._footer')
+        </div>
+    </div>
+
 </body>
 </html>
