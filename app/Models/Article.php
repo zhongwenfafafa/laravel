d2954610964472ef7e0380068e2f1f63ab71c41b
zhongwenfafafa <zwalvyuan456@gmail.com>
2018-07-23 07:22:51 +0000

    创建文章表

---
+++ app/Models/Article.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    protected $table = 'my_articles';
+
+    public function user()
+    {
+        
+    }
+}
